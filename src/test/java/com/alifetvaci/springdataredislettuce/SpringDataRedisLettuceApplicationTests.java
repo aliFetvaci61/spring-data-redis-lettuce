fe0cc5ae7cbf0edb58a4631ea9b57a61cc526322
Ali Fetvacı <ali.fetvaci@getir.com>
2023-11-10 12:30:19 +0300

    first commit

---
+++ src/test/java/com/alifetvaci/springdataredislettuce/SpringDataRedisLettuceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alifetvaci.springdataredislettuce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRedisLettuceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
